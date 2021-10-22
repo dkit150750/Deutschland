2ee7c8d06c512ec195927ab22518d0f88d98bed5
dkit150750 <dkit150750gmail.com>
2021-10-22 15:03:50 +0300

    Bewerbungen

--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-const swiper = new Swiper('.swiper', {
-	loop: true,
-	autoplay: {
-		delay: 5000,
-	},
-	clickable: true,
-
-	keyboard: {
-		enabled: true,
-		onlyInViewport: true,
-	},
-
-	pagination: {
-		el: '.swiper-pagination',
+{
+	const homeSwiper = new Swiper('.home-swiper', {
+		init: false,
+		loop: true,
+		autoplay: {
+			delay: 5000,
+		},
 		clickable: true,
-	},
 
-	breakpoints: {
-		640: {
-			navigation: {
-				nextEl: '.swiper__button-next',
-				prevEl: '.swiper__button-prev',
+		keyboard: {
+			enabled: true,
+			onlyInViewport: true,
+		},
+
+		pagination: {
+			el: '.home-swiper-pagination',
+			clickable: true,
+			bulletClass: 'home-swiper-pagination-bullet',
+			bulletActiveClass: 'home-swiper-pagination-bullet-active',
+		},
+
+		breakpoints: {
+			640: {
+				navigation: {
+					nextEl: '.home-swiper__button-next',
+					prevEl: '.home-swiper__button-prev',
+				},
 			},
 		},
-	},
-});
+	});
+
+	homeSwiper.init();
+}
---
+++ src/wild.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-undef */
+{
+	const swiperWrapper = document.querySelector('.swiper-wrapper');
+	const slides = [...swiperWrapper.querySelectorAll('.swiper-slide')];
+
+	function createColumns() {
+		const column1 = document.createElement('div');
+		const column2 = document.createElement('div');
+		column1.classList.add('wild__images-column');
+		column2.classList.add('wild__images-column');
+		column1.append(...slides.slice(0, 4));
+		column2.append(...slides.slice(4));
+		swiperWrapper.append(column1);
+		swiperWrapper.append(column2);
+	}
+
+	function removeColumns(columns) {
+		columns.forEach((column) => {
+			column.remove();
+		});
+		swiperWrapper.append(...slides);
+	}
+
+	const breakpoint = window.matchMedia('(min-width: 1140px)');
+	let wildSwiper;
+
+	const enableSwiper = () => {
+		wildSwiper = new Swiper('.wild-swiper', {
+			init: false,
+			slidesPerView: 'auto',
+			spaceBetween: 20,
+		});
+
+		wildSwiper.init();
+	};
+
+	const breakpointChecker = () => {
+		if (breakpoint.matches) {
+			if (wildSwiper !== undefined) {
+				wildSwiper.destroy(true, true);
+			}
+			createColumns();
+
+			return;
+		}
+
+		const columns = [...swiperWrapper.querySelectorAll('.wild__images-column')];
+		if (columns.length > 0) {
+			removeColumns(columns);
+		}
+
+		enableSwiper();
+	};
+
+	breakpoint.addEventListener('change', breakpointChecker);
+
+	breakpointChecker();
+}
